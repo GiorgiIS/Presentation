62be92c760630689b3b3a4bb680403a06194cdf8
george-tbcpay <g.isaiashvili@tbcpay.ge>
2021-03-18 12:12:40 +0400

    Add project files.

---
+++ ConsoleApp6/BadPractice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BadPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            var book = new Book("The lord of the rings", 5000);
+
+            book.ApplySale(3000);
+
+            Console.WriteLine(book.Name);
+        }
+    }
+
+    public class Book
+    {
+        public string Name { get; protected set; }
+
+        public long PriceInCents { get; protected set; }
+
+        public List<string> Authors { get; set; }
+
+        public Book(string name, long priceInCents)
+        {
+            Name = name;
+            PriceInCents = priceInCents;
+        }
+
+        public void ApplySale(long amount)
+        {
+            PriceInCents -= amount;
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GoodPractice
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            IBookFactory bookFactory = new BookFactory();
+
+            var defaultBook = bookFactory.Default();
+            var book = bookFactory.Create("The lord of the rings", 5000);
+
+            defaultBook.ApplySale(1000); // Throws Error
+            book.ApplySale(3000);
+        }
+    }
+
+    public class Book : ISaleableProduct, ICanHaveDefaultValue
+    {
+        private readonly bool _isDefaultValue;
+
+        private const long MinimalPossiblePriceInCents = 0;
+
+        private const string DefaultName = "Default";
+
+        public readonly string Name;
+
+        public long PriceInCents { get; protected set; }
+
+        public readonly ICollection<string> Authors = new List<string>();
+
+        public Book()
+        {
+            _isDefaultValue = true;
+            Name = DefaultName;
+            PriceInCents = MinimalPossiblePriceInCents;
+        }
+
+        public Book(string name, long priceInCents)
+        {
+            if (string.IsNullOrEmpty(name) || priceInCents <= MinimalPossiblePriceInCents)
+            {
+                throw new Exception();
+            }
+
+            _isDefaultValue = false;
+            Name = name;
+            PriceInCents = priceInCents;
+        }
+
+        public void ApplySale(long amountInCents)
+        {
+            var amountInCentsIsValid = amountInCents > MinimalPossiblePriceInCents && amountInCents <= PriceInCents;
+
+            if (!amountInCentsIsValid)
+            {
+                throw new Exception();
+            }
+
+            PriceInCents -= amountInCents;
+        }
+
+        public bool IsDefault() => _isDefaultValue;
+    }
+
+    public class BookFactory : IBookFactory
+    {
+        public Book Create(string name, long priceInCents) => new Book(name, priceInCents);
+        public Book Default() => new Book();
+    }
+
+    public interface IBookFactory : IFactory<Book>
+    {
+        Book Create(string name, long priceInCents);
+    }
+
+    public interface IFactory<T> where T : ICanHaveDefaultValue
+    {
+        T Default();
+    }
+
+    public interface ISaleableProduct
+    {
+        void ApplySale(long amountInCents);
+    }
+
+    public interface ICanHaveDefaultValue
+    {
+        bool IsDefault();
+    }
+}
